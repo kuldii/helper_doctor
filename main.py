de0ff8b77c2f0591813233a459abb2fb4125f83d
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2024-03-07 10:53:22 +0500

    feat: try to connect firestore and storage

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, storage
 import streamlit as st
-from firebase_admin import storage
+from google.cloud import firestore
+from PIL import Image
+import io
 
-cred = credentials.Certificate("service_account.json")
-
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'gs://splitproject-653ea.appspot.com'
-}, name='splitproject')
-
-# Function to get image URLs from Firebase Storage
-def get_image_urls_from_storage():
-    # Code here
-    image_urls = ""
-
-    return image_urls
+def initialize_app():
+    # Initialize Firebase SDK
+    cred = credentials.Certificate("service_account.json")
+    if not firebase_admin._apps:
+        app = firebase_admin.initialize_app(cred, {
+            'storageBucket': 'splitproject-653ea.appspot.com'
+        }, name='splitproject')
+    else:
+        app = firebase_admin.get_app('splitproject')
+    return app
 
 def main():
+    app = initialize_app()
+    
     st.title("View Images from Firebase Storage")
+    
+    # Authenticate to Firestore with the JSON account key.
+    db = firestore.Client.from_service_account_json("service_account.json")
 
-    # Get image URLs from Firebase Storage
-    image_urls = get_image_urls_from_storage()
+    # Create a reference to the Google post.
+    doc_ref = db.collection("tests").document("sItSpfHhKoDy9MEXE3KW")
 
-    # Display images
-    if image_urls:
-        st.subheader("Images from Firebase Storage")
-        for url in image_urls:
-            st.image(url, caption='Image from Firebase Storage', use_column_width=True)
+    # Then get the data at that reference.
+    doc = doc_ref.get()
+
+    # Let's see what we got!
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
+
+    # Image path in Firebase Storage
+    image_path = 'raw/2.png'
+    
+    bucket = storage.bucket(app=app)
+    blob = bucket.blob(image_path)
+    image_bytes = blob.download_as_bytes()
+    
+    if image_bytes:
+        image = Image.open(io.BytesIO(image_bytes))
+        st.image(image, caption='Image from Firebase Storage', use_column_width=True)
     else:
-        st.write("No images found in Firebase Storage.")
+        st.write("Image not found.")
 
 if __name__ == "__main__":
     main()
